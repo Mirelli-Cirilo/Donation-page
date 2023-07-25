b14f742c1f616fa504e812ad79df7a1183d41f45
Maria Mirelli Cirilo <mirellicirilo44@gmail.com>
2023-07-24 21:16:16 -0300

    .gitignore

---
+++ keys.py
@@ -0,0 +1,5 @@
+key = {
+    'STRIPE_PUBLIC_KEY_TEST': 'pk_test_51NVYIzLOemdyCdBn9Y8iWIDJTsL2P2DLP1Y4xYbyHLLsQ4bqiS5N7GudXmntCh5guqRCiEwWgS4q3qTilq8w1dKu0047ZO6BwX',
+    'STRIPE_SECRET_KEY_TEST': 'sk_test_51NVYIzLOemdyCdBnpzGsT3UBqxyMDeM8n1yjjtEUoHSDvX9MbZ6LcCApSUCrYjqWzEGBzyqM8k9FX66WQs3jfDcX00uZufrKRk',
+    'SECRET_KEY': 'django-insecure-@!ajdveuoc&u=+@a#!er!@i2x8)(f+xzz#&s1%=1u^*)!jc$%o'
+}
